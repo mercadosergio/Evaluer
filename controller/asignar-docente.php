da387fe7cf6f64477c70fa26633605bebc8dd52d
Sergio1-Git <mercadosergio754@gmail.com>
2022-05-15 20:04:56 -0500

    fix controller

--- controller/asignar-docente.php
+++ controller/asignar-docente.php
@@ -2,34 +2,34 @@
 
 include("../model/conexion.php");
 
-if (isset($_POST['asignar_d'])) {
 
 
-    $id_p = $_POST['id_proyecto'];
-    $asesor = $_POST['asesor'];
+$id_p = $_POST['id_proyecto'];
+$asesor = $_POST['asesor'];
 
+$json = json_encode($asesor, true);
 
+for ($i = 0; $i < count($asesor); $i++) {
 
-    $json = json_encode($asesor, true);
-
-    for ($i = 0; $i < count($asesor); $i++) {
-
-        $var_asesor = $asesor[$i];
-        $conexion->query("UPDATE proyecto_grado SET asesor_user = '$var_asesor' WHERE id = '$id_p'");
-        $conexion->query("UPDATE proyecto_grado p JOIN DOCENTE d
+    $var_asesor = $asesor[$i];
+    $conexion->query("UPDATE proyecto_grado SET asesor_user = '$var_asesor' WHERE id = '$id_p'");
+    $conexion->query("UPDATE proyecto_grado p JOIN DOCENTE d
         ON p.asesor_user = d.usuario
         SET p.nombre_asesor = concat(d.nombres,d.p_apellido)");
-    }
-?>
-    <p style="position: absolute; padding: 10px;border-radius: 10px; top: 20%; left: 650px;opacity: 1;
-    text-align: center; width: 20%; background: #abff96; color: #1e9700; border: 1px #1e9700 solid;" id="success">Asesor asignado</p>
-    <script>
-        setTimeout(function() {
-            $('#success').fadeOut('fast');
-        }, 2000); // <-- time in milliseconds
-    </script>
-
-<?php
-    header("location: ../coordinador/asignar-asesor.php");
-    mysqli_close($conexion);
 }
+?>
+<p style="position: absolute; padding: 10px;border-radius: 10px; top: 20%; left: 650px;opacity: 1;
+    text-align: center; width: 20%; background: #abff96; color: #1e9700; border: 1px #1e9700 solid;" id="success">Asesor asignado</p>
+<script>
+    setTimeout(function() {
+        $('#success').fadeOut('fast');
+    }, 2000); // <-- time in milliseconds
+</script>
+<script>
+    window.location = "../pages/coordinador/asignar-asesor.php";
+</script>
+<?php
+// header("location: ../pages/coordinador/asignar-asesor.php");
+// include("../pages/coordinador/asignar-asesor.php");
+mysqli_close($conexion);
+?>
--- pages/coordinador/asignar-asesor.php
+++ pages/coordinador/asignar-asesor.php
@@ -27,6 +27,8 @@ include("../../controller/nombre.php");
     <link rel="stylesheet" href="../../css/unicons.css">
     <link rel="stylesheet" href="../../css/owl.carousel.min.css">
     <link rel="stylesheet" href="../../css/owl.theme.default.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -67,66 +69,115 @@ include("../../controller/nombre.php");
             </div>
         </div>
     </nav>
-    <h3 class="title">Asignar asesor</h3>
-    <table class="tabla_asesor">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Título</th>
-                <th>Documento</th>
-                <th>Programa</th>
-                <th>Semestre</th>
-                <th>Fecha y hora</th>
-                <th>Asesor</th>
-                <th>Acción</th>
-            </tr>
-        </thead>
-        <?php
-        include("../../controller/asignar-docente.php");
-        ?>
-        <form method="POST">
-            <?php
-            $sql = "SELECT * from proyecto_grado";
-            $result = mysqli_query($conexion, $sql);
 
-
-            while ($mostrar = mysqli_fetch_array($result)) {
-            ?>
+    <div class="main_section">
+        <h3 class="title">Asignar asesor</h3>
+        <table class="tabla_asesor">
+            <thead>
                 <tr>
-                    <td><?php echo $mostrar['0'] ?></td>
-                    <td style="max-width: 600px;"><?php echo $mostrar['titulo'] ?></td>
-                    <td><a href="<?php echo $mostrar['documento']; ?>"><?php echo $mostrar['nombre']; ?></a></td>
-                    <td><?php echo $mostrar['programa'] ?></td>
-                    <td style="text-align:center;"><?php echo $mostrar['semestre'] ?></td>
-                    <td><?php echo $mostrar['fecha'] ?></td>
-
-                    <td>
-                        <select name="asesor[]">
-                            <option selected value="<?php echo $mostrar['asesor_user']; ?>"><?php echo $mostrar['nombre_asesor']; ?></option>
-                            <option value="1">Seleccione...</option>
-                            <?php
-                            $buscar_docente = "SELECT * FROM docente";
-                            $resultado = mysqli_query($conexion, $buscar_docente);
-
-                            while ($filas = mysqli_fetch_array($resultado)) {
-                                echo '<option value="' . $filas['usuario'] . '">' . $filas['nombres'] . '</option>';
-                            }
-                            ?>
-                        </select>
-                    </td>
-                    <td>
-                        <input type="text" hidden name="id_proyecto" value="<?php echo $mostrar['0'] ?>">
-                        <input class="asignar" name="asignar_d" value="Guardar" type="submit">
-                    </td>
+                    <th>#</th>
+                    <th>Título</th>
+                    <th>Documento</th>
+                    <th>Programa</th>
+                    <th>Semestre</th>
+                    <th>Fecha y hora</th>
+                    <th>Asesor de proyecto</th>
                 </tr>
-
+            </thead>
             <?php
-            }
+            // include("../../controller/asignar-docente.php");
             ?>
-        </form>
-    </table>
+            <form id="form" action="../../controller/asignar-docente.php" name="sub" method="POST">
+                <?php
+                $sql = "SELECT * from proyecto_grado";
+                $result = mysqli_query($conexion, $sql);
 
 
+                while ($mostrar = mysqli_fetch_array($result)) {
+                ?>
+                    <tr>
+                        <td><?php echo $mostrar['0'] ?></td>
+                        <td style="max-width: 600px;"><?php echo $mostrar['titulo'] ?></td>
+                        <td><a href="<?php echo $mostrar['documento']; ?>"><?php echo $mostrar['nombre']; ?></a></td>
+                        <td><?php echo $mostrar['programa'] ?></td>
+                        <td style="text-align:center;"><?php echo $mostrar['semestre'] ?></td>
+                        <td><?php echo $mostrar['fecha'] ?></td>
+
+                        <td>
+                            <select name="asesor[]" id="asesor" onchange="seleccionarDocente()">
+                                <option selected value="<?php echo $mostrar['asesor_user']; ?>"><?php echo $mostrar['nombre_asesor']; ?></option>
+                                <option value="1">Seleccione...</option>
+                                <?php
+                                $buscar_docente = "SELECT * FROM docente";
+                                $resultado = mysqli_query($conexion, $buscar_docente);
+
+                                while ($filas = mysqli_fetch_array($resultado)) {
+                                    echo '<option value="' . $filas['usuario'] . '">' . $filas['nombres'] . " " . $filas['p_apellido']  . '</option>';
+                                }
+                                ?>
+                            </select>
+                        </td>
+                        <td hidden>
+                            <input type="text" hidden id="id_proyecto" name="id_proyecto" value="<?php echo $mostrar['0'] ?>">
+                            <input class="asignar" name="asignar_d" value="Guardar" type="submit">
+                        </td>
+                    </tr>
+
+                <?php
+                }
+                ?>
+            </form>
+        </table>
+    </div>
+
+    <script type="text/javascript">
+        function seleccionarDocente() {
+
+            let id = document.getElementById('id_proyecto').value;
+            let asesor = document.getElementById('asesor');
+            let variables = asesor.value;
+
+            const formulario = document.getElementById("asesor");
+
+
+            formulario.addEventListener("submit", (e) => {
+                e.preventDefault();
+                const request = new XMLHttpRequest();
+
+                request.open("post", "../../controller/asignar-docente.php");
+                request.onload = function() {
+                    console.log(request.responseText);
+                }
+                request.send(new FormData(formulario));
+            });
+
+            document.sub.submit();
+            // alert(id);
+            // alert(variables);
+            // document.sub.submit();
+
+        }
+    </script>
+
+
+    <script>
+        // var cod = document.getElementById("asesor").value;
+        // // alert(cod);
+
+        // /* Para obtener el texto */
+        // var combo = document.getElementById("asesor");
+        // var selected = combo.options[combo.selectedIndex].text;
+        // // alert(selected);
+
+        // const $form = document.querySelector('#form')
+        // $form.addEventListener('submit', handleSubmit)
+
+        // function handleSubmit(event) {
+        //     event.preventDefault()
+        //     const form = new FormData(this)
+        //     console.log(cod)
+        // }
+    </script>
     <script src="../../js/jquery-3.3.1.min.js"></script>
     <script src="../../js/popper.min.js"></script>
     <script src="../../js/bootstrap.min.js"></script>
